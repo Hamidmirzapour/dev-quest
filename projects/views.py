2a9fd258fee41ec4df126ed298d1c0f60c1fb386
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2024-08-11 10:19:04 +0330

    Add projects application to settings of project, Add projects urls to the urls of project, and add first urls and views logic to test

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def get_projects(request):
+    return HttpResponse('All the projects to show')
