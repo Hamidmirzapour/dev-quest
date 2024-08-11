1d068383ae479d8736e24951c9fabe3e003a6c92
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2024-08-11 12:35:11 +0330

    Create a project model and migrate it to create project table in database and add this model to django admin panel

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2180, null=True, blank=True)
+    source_code = models.CharField(max_length=2180, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
